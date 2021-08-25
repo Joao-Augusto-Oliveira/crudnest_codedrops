1ae0301543e9bec60acfa899d1262887bdaed040
Joao Augusto Oliveira dos Santos <joaoaugustofurg@gmail.com>
2021-08-24 22:10:12 -0300

    Heroku

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  // verificar o módulo do mongo
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://joao:Jm@200413@cluster0.yjal6.mongodb.net/test',
+    ),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
